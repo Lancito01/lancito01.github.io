14676800127e4fbf4526145ab735dee88089ae3e
Andy <68301335+Lancito01@users.noreply.github.com>
2023-05-23 01:02:06 -0300

    changed placing of texts (completely changed aesthetic of website), added animations, re-made some code so it's cleaner and easier to work on

--- js/script.js
+++ js/script.js
@@ -1,67 +1,79 @@
 let ModeStatus = "dark"
-let HtmlElement = document.documentElement;
+let HtmlElement = document.documentElement
+let AccentColorDark = "cyan", AccentColorLight = "red"
 
 // age calculator 1073530800
 document.addEventListener("DOMContentLoaded", function () {
     let result = Math.floor((Date.now() / 1000 - 1073530800) / 31536000)
-    let ageSpan = document.querySelector(".age");
+    let ageSpan = document.querySelector(".age")
     if (ageSpan) {
-        ageSpan.textContent = result.toString();
+        ageSpan.textContent = result.toString()
     } else {
-        console.error("Could not find span element with class 'age'");
+        console.error("Could not find span element with class 'age'")
     }
-});
+})
 
 function toggleDarkLightMode(status) {
-    if (status === "dark") {
-        HtmlElement.classList.remove("light-mode")
-        HtmlElement.classList.add("dark-mode")
-    }
-    else if (status === "light") {
-        HtmlElement.classList.remove("dark-mode");
-        HtmlElement.classList.add("light-mode");
+    switch (status) {
+        case "dark":
+            HtmlElement.classList.remove("light-mode")
+            HtmlElement.classList.add("dark-mode")
+            break
+        case "light":
+            HtmlElement.classList.remove("dark-mode")
+            HtmlElement.classList.add("light-mode")
+            break
+        default:
+            break
     }
 }
 
 function darkModeFunction() {
-    if (ModeStatus === "dark") {
-        ModeStatus = "light"
-    }
-    else if (ModeStatus === "light") {
-        ModeStatus = "dark"
+    switch (ModeStatus) {
+        case "dark":
+            ModeStatus = "light"
+            break
+        case "light":
+            ModeStatus = "dark"
+            break
+        default:
+            break
     }
 
-    let arrows = document.querySelectorAll(".arrow-color")
+    let accentColorElements = document.querySelectorAll(".accent-color")
     let button = document.querySelector(".mode-toggle-button")
     let nav = document.querySelector("nav")
-    nav.style.transition = "0.5s"
 
-    if (ModeStatus === "dark") { // toggled dark mode
-        toggleDarkLightMode(ModeStatus)
+    switch (ModeStatus) {
+        case "dark":
+            toggleDarkLightMode(ModeStatus)
 
-        for (let index = 0; index < arrows.length; index++) {
-            arrows[index].style.color = "cyan"
-        }
+            for (let index = 0; index < accentColorElements.length; index++) {
+                accentColorElements[index].style.color = "cyan"
+            }
 
-        nav.style.backgroundColor = "#141414"
-        nav.style.color = "white"
-        nav.style.borderBottom = "1px solid white"
+            nav.style.backgroundColor = "#141414"
+            nav.style.color = "white"
+            nav.style.borderBottom = "1px solid " + AccentColorDark
 
-        button.style.backgroundColor = "cyan"
-        button.style.color = "black"
-    }
-    else if (ModeStatus === "light") { // toggled light mode
-        toggleDarkLightMode(ModeStatus)
+            button.style.backgroundColor = "#1461ff"
+            button.style.color = "white"
+            break
+        case "light":
+            toggleDarkLightMode(ModeStatus)
 
-        for (let index = 0; index < arrows.length; index++) {
-            arrows[index].style.color = "red"
-        }
+            for (let index = 0; index < accentColorElements.length; index++) {
+                accentColorElements[index].style.color = "red"
+            }
 
-        nav.style.backgroundColor = "white"
-        nav.style.color = "black"
-        nav.style.borderBottom = "1px solid black"
+            nav.style.backgroundColor = "white"
+            nav.style.color = "black"
+            nav.style.borderBottom = "1px solid " + AccentColorLight
 
-        button.style.backgroundColor = "red"
-        button.style.color = "black"
+            button.style.backgroundColor = "#ffa34b"
+            button.style.color = "black"
+            break
+        default:
+            break
     }
 }
