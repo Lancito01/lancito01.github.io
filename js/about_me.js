c1c7008f45a5fc9175877b51d368e6e4435cf082
Andy <68301335+Lancito01@users.noreply.github.com>
2023-08-11 22:16:24 -0300

    forgot to stage other files, oops

---
+++ js/about_me.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let result = Math.floor((Date.now() / 1000 - 1073530800) / 31536000)
+    let ageSpan = document.querySelector(".age")
+    if (ageSpan) {
+        ageSpan.textContent = result.toString()
+    } else {
+        console.error("Could not find span element with class 'age'")
+    }
+})
